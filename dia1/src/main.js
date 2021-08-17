4f96ba8b13bf94f5a0730a3045248f76045606a9
Saull Brandão <saull@outlook.com>
2021-08-17 11:53:31 -0300

    feat: add a link to hide/show the app div on click

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,14 @@
 import "./style.css";
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]');
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+const link = document.querySelector('[data-js="link"]');
+
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+  app.classList.toggle("hidden");
+});
