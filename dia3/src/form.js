9f4ce7e6dac0d3796b3156af57f81e60bf9fad4f
Saull Brandão <saull@outlook.com>
2021-08-19 15:56:25 -0300

    feat: show elements with the colors selected

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,33 @@
 const input = document.querySelector('[data-js="name"]');
+const form = document.querySelector('[data-js="form"]');
+
+const colorSelect = document.createElement("select");
+colorSelect.setAttribute("multiple", true);
+colorSelect.className = "color-select";
+form.appendChild(colorSelect);
+
+const selectContainer = document.createElement("div");
+selectContainer.className = "container";
+document.body.appendChild(selectContainer);
+
+["red", "blue", "yellow", "green", "black"].map((color) => {
+  const el = document.createElement("option");
+  el.value = color;
+  el.textContent = color;
+  el.dataset.js = `option-${color}`;
+  colorSelect.appendChild(el);
+});
+
+colorSelect.addEventListener("change", (event) => {
+  [...document.querySelectorAll(`.color-div`)].map((el) => el.remove());
+
+  [...event.target.selectedOptions].map((el) => {
+    const colorDiv = document.createElement("div");
+    colorDiv.className = `color-div ${el.value}`;
+    colorDiv.dataset.js = `color-${el.value}`;
+    selectContainer.appendChild(colorDiv);
+  });
+});
 
 function capitalize(word) {
   const newWord = word.trim().split("");
