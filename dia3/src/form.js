8d0143c059db3eef1b09721efda672399281d06e
Saull Brandão <saull@outlook.com>
2021-08-20 09:25:20 -0300

    refactor: color selector

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,33 +1,4 @@
 const nameInput = document.querySelector('[data-js="name"]');
-const form = document.querySelector('[data-js="form"]');
-
-const colorSelect = document.createElement("select");
-colorSelect.setAttribute("multiple", true);
-colorSelect.className = "color-select";
-form.appendChild(colorSelect);
-
-const selectContainer = document.createElement("div");
-selectContainer.className = "container";
-document.body.appendChild(selectContainer);
-
-["red", "blue", "yellow", "green", "black"].map((color) => {
-  const el = document.createElement("option");
-  el.value = color;
-  el.textContent = color;
-  el.dataset.js = `option-${color}`;
-  colorSelect.appendChild(el);
-});
-
-colorSelect.addEventListener("change", (event) => {
-  [...document.querySelectorAll(`.color-div`)].map((el) => el.remove());
-
-  [...event.target.selectedOptions].map((el) => {
-    const colorDiv = document.createElement("div");
-    colorDiv.className = `color-div ${el.value}`;
-    colorDiv.dataset.js = `color-${el.value}`;
-    selectContainer.appendChild(colorDiv);
-  });
-});
 
 const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
 
@@ -49,4 +20,42 @@ nameInput.addEventListener("input", (event) => {
         : capitalize(word.toLowerCase())
     )
     .join(" ");
+});
+
+const form = document.querySelector('[data-js="form"]');
+
+const colorSelect = document.createElement("select");
+colorSelect.setAttribute("multiple", "");
+colorSelect.className = "color-select";
+form.appendChild(colorSelect);
+
+const colorsContainer = document.createElement("div");
+colorsContainer.className = "container";
+form.insertAdjacentElement("afterend", colorsContainer);
+
+["red", "blue", "yellow", "green", "black"].map((color) => {
+  colorSelect.appendChild(createColorOption(color));
+});
+
+function createColorOption(color) {
+  const el = document.createElement("option");
+  el.value = color;
+  el.textContent = color;
+  el.dataset.js = `option-${color}`;
+  return el;
+}
+
+function createColorDiv(value) {
+  const colorDiv = document.createElement("div");
+  colorDiv.className = `color-div ${value}`;
+  colorDiv.dataset.js = `color-${value}`;
+  return colorDiv;
+}
+
+colorSelect.addEventListener("change", (event) => {
+  colorsContainer.innerHTML = "";
+
+  [...event.target.selectedOptions].map((el) => {
+    colorsContainer.appendChild(createColorDiv(el.value));
+  });
 });
